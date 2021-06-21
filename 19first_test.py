80a56c3b37b8229bd6e0038b1638dfa0b7c955bb
ALEKSEI RUKINOV <58972919+Alruk-art@users.noreply.github.com>
2021-06-21 20:27:03 +0300

    Add files via upload

---
+++ 19first_test.py
@@ -0,0 +1,19 @@
+import pytest
+from app19.calclator import Calcr
+
+
+class TestCalc: # слово Test обязательно
+    def setup(self):
+        self.calc = Calcr
+
+    def test_multiply_calculate_correctly(self):
+        assert self.calc.multiply(self,2, 3) == 6
+
+    def test_division_calculate_OK(self):
+        assert self.calc.division(self,15, 3) == 5
+
+    def test_subtraction_calculate_YES(self):
+        assert self.calc.subtraction(self,10, 4) == 6
+
+    def test_adding_calculate_correct(self):
+        assert self.calc.adding(self,10, 4) == 14
