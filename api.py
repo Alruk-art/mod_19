f12073e74786a3de9af2936d91394696caaad9ae
ALEKSEI RUKINOV <58972919+Alruk-art@users.noreply.github.com>
2021-06-25 22:43:38 +0300

    Update api.py

--- api.py
+++ api.py
@@ -6,7 +6,7 @@ from requests_toolbelt.multipart.encoder import MultipartEncoder
 
 
 class PetFriends:
-    """апи библиотека к веб приложению Pet Friends"""
+    """api библиотека к веб приложению Pet Friends"""
 
     def __init__(self):
         self.base_url = "https://petfriends1.herokuapp.com/"
@@ -30,7 +30,7 @@ class PetFriends:
 
     def get_list_of_pets(self, auth_key: json, filter: str = "") -> json:
         """Метод делает запрос к API сервера и возвращает статус запроса и результат в формате JSON
-        со списком наденных питомцев, совпадающих с фильтром. На данный момент фильтр может иметь
+        со списком найденных питомцев, совпадающих с фильтром. На данный момент фильтр может иметь
         либо пустое значение - получить список всех питомцев, либо 'my_pets' - получить список
         собственных питомцев"""
 
@@ -48,7 +48,7 @@ class PetFriends:
 
     def add_new_pet(self, auth_key: json, name: str, animal_type: str,
                     age: str, pet_photo: str) -> json:
-        """Метод отправляет (постит) на сервер данные о добавляемом питомце и возвращает статус
+        """Метод post отправляет (постит) на сервер данные о добавляемом питомце и возвращает статус
         запроса на сервер и результат в формате JSON с данными добавленного питомца"""
 
         data = MultipartEncoder(
@@ -89,7 +89,7 @@ class PetFriends:
 
     def update_pet_info(self, auth_key: json, pet_id: str,  name: str,
                         animal_type: str, age: int) -> json:
-        """Метод отправляет запрос на сервер о обновлении данных питомуа по указанному ID и
+        """Метод put отправляет запрос на сервер о обновлении данных питомца по указанному ID и
         возвращает статус запроса и result в формате JSON с обновлённыи данными питомца"""
 
         headers = {'auth_key': auth_key['key']}
@@ -113,8 +113,8 @@ class PetFriends:
 #  мои записи
     def add_new_pet_simple(self, auth_key: json, name: str, animal_type: str,
                                     age: str) -> json:
-        """Метод отправляет (постит) на сервер данные о добавляемом питомце и возвращает статус
-        запроса на сервер и результат в формате JSON с данными добавленного питомца"""
+        """Метод post отправляет (постит) на сервер данные о добавляемом питомце без фото и возвращает статус
+        запроса на сервер и результат в формате JSON с данными добавленного питомца. """
 
         data = MultipartEncoder(
             fields={
@@ -135,7 +135,9 @@ class PetFriends:
 
 
     def add_photo_pet(self, auth_key: json, pet_id: str,  pet_photo: str) -> json:
-
+        
+        """Метод post отправляет (постит) на сервер фото питомца """
+        
         data = MultipartEncoder(
             fields={
                 'pet_photo': (pet_photo, open(pet_photo, 'rb'), 'image/jpg')
